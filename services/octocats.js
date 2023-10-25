e4d9076c7354cd42ca11b629f09564892c6d3fa3
Johannes Nicolai <johannes.nicolai@postman.com>
2023-10-25 13:56:14 +0200

    Remove Simon temporarily

--- services/octocats.js
+++ services/octocats.js
@@ -1117,7 +1117,6 @@ module.exports = {
           },
           {
             "authors": [
-              "Simon Oxley"
             ],
             "octocat": "https://octodex.github.com/images/original.png",
             "title": "Original"
